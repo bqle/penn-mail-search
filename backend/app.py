9a62c9184c874bbd88aecfa3928f278aa10ac183
bqle <benjaminle2002@gmail.com>
2022-02-05 07:46:51 -0500

    Hello world testing on EC2

--- backend/app.py
+++ backend/app.py
@@ -18,5 +18,9 @@ def search():
     result = mongo_driver.search(name=name, school=school)
     return result
     
+@app.route("/")
+def home():
+    return "Hello world"
+
 if __name__ == '__main__':
     app.run()
